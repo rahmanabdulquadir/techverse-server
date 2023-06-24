65e8d94cef726b5d16b0a9b871cf3eb5990d687c
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-06-24 16:11:03 +0600

    project updated

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
+require('dotenv').config()
 
-const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
   res.send('hello from techverse server')
 })
 
-app.get('/courses', (req, res) => {
-  res.send(courses)
-})
 
-app.get('/courses/:id', (req, res) => {
-  const id = req.params.id
-  const course = courses.find(cr => cr.id === id)
-  res.send(course)
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rl1a5mc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run () {
+  try{
+    const courseCollection = client.db("TechVerse").collection('courses');
+
+    app.get('/courses', async(req, res) => {
+      const query = {}
+      const cursor = courseCollection.find(query)
+      const courses = await cursor.toArray()
+      res.send(courses)
+    })
+
+    app.get('/courses/:id', async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const course = await courseCollection.findOne(query)
+      res.send(course)
+    })
+  }
+  finally{
+
+  }
+}
+
+run().catch((err) => console.log(err))
+
+
 
 app.listen(port, () => {
   console.log('This server is running from port', port)
